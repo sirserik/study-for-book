919710da6996f2aba3d95c19ced1e0cf0e43dd69
sirserik <serik@example.com>
2026-05-08 08:51:13 +0500

    Initial Commit

---
+++ study-for-book/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  study-for-book
+//
+//  Created by Serik on 08.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
